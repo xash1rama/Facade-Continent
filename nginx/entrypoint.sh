35759fc667979a50cdc040fd5bfebbd28fa4c0d7
Vlad Asmakovets <165921625+xash1rama@users.noreply.github.com>
2025-03-14 17:54:03 +0300

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,11 @@
 DOMAIN="facade-continent.ru"
-EMAIL="facadecontinent@gmail.com"  # Замените на ваш email для получения уведомлений
+EMAIL="facadecontinent@gmail.com"
 
 # Получение сертификатов
-certbot certonly --nginx -d $DOMAIN -d www.$DOMAIN --email $EMAIL --agree-tos --non-interactive
+if ! certbot certonly --nginx -d $DOMAIN -d www.$DOMAIN --email $EMAIL --agree-tos --non-interactive; then
+    echo "Ошибка при получении сертификатов!"
+    exit 1
+fi
 
 # Запуск Nginx
 nginx -g 'daemon off;'
